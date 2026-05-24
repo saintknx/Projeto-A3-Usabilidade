0c3cecc06a6d372ab910d253041804b251beb481
Kaua Sousa <kauasantos2014s10@gmail.com>
2026-05-24 09:58:09 -0300

    feat: mapeamento de cores por tipo e tradução de nomes

---
+++ src/utils/typeColors.js
@@ -0,0 +1,59 @@
+/**
+ * Cores mocadas de cada tipo de Pokémon
+ */
+export const TYPE_COLORS = {
+    fire:     "#FF6B35",
+    water:    "#4F9EE8",
+    grass:    "#56C363",
+    electric: "#FFD700",
+    ice:      "#96D9E8",
+    fighting: "#C03028",
+    poison:   "#A040A0",
+    ground:   "#E0C068",
+    flying:   "#A890F0",
+    psychic:  "#F85888",
+    bug:      "#A8B820",
+    rock:     "#B8A038",
+    ghost:    "#705898",
+    dragon:   "#7038F8",
+    dark:     "#705848",
+    steel:    "#B8B8D0",
+    fairy:    "#EE99AC",
+    normal:   "#A8A878",
+};
+
+/**
+ * Tradução dos tipos
+ */
+export const TYPE_LABELS_PT = {
+    fire:     "Fogo",
+    water:    "Água",
+    grass:    "Planta",
+    electric: "Elétrico",
+    ice:      "Gelo",
+    fighting: "Lutador",
+    poison:   "Venenoso",
+    ground:   "Terra",
+    flying:   "Voador",
+    psychic:  "Psíquico",
+    bug:      "Inseto",
+    rock:     "Pedra",
+    ghost:    "Fantasma",
+    dragon:   "Dragão",
+    dark:     "Sombrio",
+    steel:    "Aço",
+    fairy:    "Fada",
+    normal:   "Normal",
+    };
+
+/**
+ * Tradução dos nomes dos stats
+ */
+export const STAT_LABELS_PT = {
+    hp:               "HP",
+    attack:           "Ataque",
+    defense:          "Defesa",
+    "special-attack": "Sp. Atq",
+    "special-defense":"Sp. Def",
+    speed:            "Velocidade",
+};
